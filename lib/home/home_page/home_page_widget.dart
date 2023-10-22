612c1a916fafef56568303453e20cc2788d9b7e0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-22 17:25:55 +0000

    Updating to latest FlutterFlow output.

--- lib/home/home_page/home_page_widget.dart
+++ lib/home/home_page/home_page_widget.dart
@@ -72,20 +72,6 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                 .toString(),
           );
         });
-        await showDialog(
-          context: context,
-          builder: (alertDialogContext) {
-            return AlertDialog(
-              title: Text('1'),
-              actions: [
-                TextButton(
-                  onPressed: () => Navigator.pop(alertDialogContext),
-                  child: Text('Ok'),
-                ),
-              ],
-            );
-          },
-        );
       } else {
         await showDialog(
           context: context,
