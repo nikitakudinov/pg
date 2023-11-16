41ed7bb2ca39e5011c01e683678beea67af65622
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-16 18:26:45 +0000

    Updating to latest FlutterFlow output.

--- lib/team_add/team_add_widget.dart
+++ lib/team_add/team_add_widget.dart
@@ -190,34 +190,32 @@ class _TeamAddWidgetState extends State<TeamAddWidget> {
                       ),
                     ),
                   ),
-                  if (_model.uploadedFileUrl !=
-                      'https://supabase.proplayclub.ru/storage/v1/object/public/playground/teamLogos/image-7XR1sw6U%20-%20transformed%20(1).png')
-                    FFButtonWidget(
-                      onPressed: () async {
-                        await deleteSupabaseFileFromPublicUrl(
-                            _model.uploadedFileUrl);
-                      },
-                      text: 'Удалить',
-                      options: FFButtonOptions(
-                        width: 100.0,
-                        padding: EdgeInsetsDirectional.fromSTEB(
-                            10.0, 0.0, 10.0, 0.0),
-                        iconPadding:
-                            EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                        color: FlutterFlowTheme.of(context).primary,
-                        textStyle:
-                            FlutterFlowTheme.of(context).titleSmall.override(
-                                  fontFamily: 'Cabin Condensed',
-                                  color: Colors.white,
-                                ),
-                        elevation: 3.0,
-                        borderSide: BorderSide(
-                          color: Colors.transparent,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(3.0),
+                  FFButtonWidget(
+                    onPressed: () async {
+                      await deleteSupabaseFileFromPublicUrl(
+                          _model.uploadedFileUrl);
+                    },
+                    text: 'Удалить',
+                    options: FFButtonOptions(
+                      width: 100.0,
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
+                      iconPadding:
+                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                      color: FlutterFlowTheme.of(context).primary,
+                      textStyle:
+                          FlutterFlowTheme.of(context).titleSmall.override(
+                                fontFamily: 'Cabin Condensed',
+                                color: Colors.white,
+                              ),
+                      elevation: 3.0,
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                        width: 1.0,
                       ),
+                      borderRadius: BorderRadius.circular(3.0),
                     ),
+                  ),
                 ],
               ),
               Padding(
