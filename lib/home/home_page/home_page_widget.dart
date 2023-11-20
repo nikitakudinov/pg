7c2377dc2729af178e3d1ce6c126efb0f4098185
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-20 18:19:16 +0000

    Updating to latest FlutterFlow output.

--- lib/home/home_page/home_page_widget.dart
+++ lib/home/home_page/home_page_widget.dart
@@ -34,7 +34,6 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      await action_blocks.loadAuthUserAlerts(context);
       await action_blocks.upadateAuthUserDataValues(context);
       await action_blocks.loadAuthUserChats(context);
       setState(() {});
