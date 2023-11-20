62c1403822109c3b98799e630b747ace1edf3669
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-20 20:27:42 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -950,7 +950,9 @@ class UseralertsCall {
         'Authorization':
             'BearereyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE',
       },
-      params: {},
+      params: {
+        'UID': uid,
+      },
       returnBody: true,
       encodeBodyUtf8: false,
       decodeUtf8: false,
