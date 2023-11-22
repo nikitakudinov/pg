147554a67204730c71b8390a54d89cdae8c84674
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-22 17:33:45 +0000

    Updating to latest FlutterFlow output.

--- lib/home/home_page/home_page_widget.dart
+++ lib/home/home_page/home_page_widget.dart
@@ -5,7 +5,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/flutter_flow/instant_timer.dart';
 import '/actions/actions.dart' as action_blocks;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -34,17 +33,9 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      await action_blocks.preloadDataOfHomePage(context);
-      setState(() {
-        FFAppState().allTEAMScount = 0;
-      });
-      _model.instantTimer = InstantTimer.periodic(
-        duration: Duration(milliseconds: 10000),
-        callback: (timer) async {
-          await action_blocks.notificationsUpdater(context);
-        },
-        startImmediately: true,
-      );
+      if (FFAppState().authPlayer == null) {
+        await action_blocks.authPlayerUpdater(context);
+      }
     });
   }
 
