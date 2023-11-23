b68e220fcaa9f41e2a3a093a77d0ef9c88986a0b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-23 16:47:38 +0000

    Updating to latest FlutterFlow output.

--- lib/team/teams/teams_widget.dart
+++ lib/team/teams/teams_widget.dart
@@ -821,34 +821,40 @@ class _TeamsWidgetState extends State<TeamsWidget>
                                 Column(
                                   mainAxisSize: MainAxisSize.max,
                                   children: [
-                                    FFButtonWidget(
-                                      onPressed: () {
-                                        print('Button pressed ...');
-                                      },
-                                      text: 'В чат команды',
-                                      icon: Icon(
-                                        Icons.groups,
-                                        size: 15.0,
-                                      ),
-                                      options: FFButtonOptions(
-                                        width: 140.0,
-                                        height: 30.0,
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            8.0, 0.0, 8.0, 0.0),
-                                        iconPadding:
-                                            EdgeInsetsDirectional.fromSTEB(
-                                                0.0, 0.0, 0.0, 0.0),
-                                        color: FlutterFlowTheme.of(context)
-                                            .tertiary,
-                                        textStyle: FlutterFlowTheme.of(context)
-                                            .bodySmall,
-                                        elevation: 3.0,
-                                        borderSide: BorderSide(
-                                          color: Colors.transparent,
-                                          width: 1.0,
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0.0, 0.0, 0.0, 5.0),
+                                      child: FFButtonWidget(
+                                        onPressed: () {
+                                          print('Button pressed ...');
+                                        },
+                                        text: 'В чат команды',
+                                        icon: Icon(
+                                          Icons.groups,
+                                          size: 15.0,
                                         ),
-                                        borderRadius:
-                                            BorderRadius.circular(4.0),
+                                        options: FFButtonOptions(
+                                          width: 140.0,
+                                          height: 30.0,
+                                          padding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  8.0, 0.0, 8.0, 0.0),
+                                          iconPadding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  0.0, 0.0, 0.0, 0.0),
+                                          color: FlutterFlowTheme.of(context)
+                                              .tertiary,
+                                          textStyle:
+                                              FlutterFlowTheme.of(context)
+                                                  .bodySmall,
+                                          elevation: 3.0,
+                                          borderSide: BorderSide(
+                                            color: Colors.transparent,
+                                            width: 1.0,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(4.0),
+                                        ),
                                       ),
                                     ),
                                     FFButtonWidget(
