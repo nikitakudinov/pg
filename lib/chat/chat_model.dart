1f7fb4a5227940fd26778856780ad4bd6b2753ac
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-29 12:23:50 +0000

    Updating to latest FlutterFlow output.

--- lib/chat/chat_model.dart
+++ lib/chat/chat_model.dart
@@ -1,12 +1,15 @@
 import '/auth/supabase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'chat_widget.dart' show ChatWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -28,6 +31,10 @@ class ChatModel extends FlutterFlowModel<ChatWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // Stores action output result for [Backend Call - API (GETCHATMESSAGES)] action in CHAT widget.
+  ApiCallResponse? apiResulttpi;
+  // Stores action output result for [Custom Action - dtMSG] action in CHAT widget.
+  List<MessageStruct>? dtMSGdata;
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode;
   TextEditingController? textController;
--- lib/chat/chat_widget.dart
+++ lib/chat/chat_widget.dart
@@ -1,11 +1,14 @@
 import '/auth/supabase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -35,6 +38,36 @@ class _ChatWidgetState extends State<ChatWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => ChatModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.apiResulttpi = await MessagingGroup.getchatmessagesCall.call(
+        chatId: widget.chatID?.toString(),
+      );
+      if ((_model.apiResulttpi?.succeeded ?? true)) {
+        _model.dtMSGdata = await actions.dtMSG(
+          (_model.apiResulttpi?.jsonBody ?? ''),
+        );
+        setState(() {
+          FFAppState().messages =
+              _model.dtMSGdata!.toList().cast<MessageStruct>();
+        });
+        await showDialog(
+          context: context,
+          builder: (alertDialogContext) {
+            return AlertDialog(
+              title: Text('as'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(alertDialogContext),
+                  child: Text('Ok'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    });
 
     _model.textController ??= TextEditingController();
     _model.textFieldFocusNode ??= FocusNode();
@@ -120,145 +153,130 @@ class _ChatWidgetState extends State<ChatWidget> {
             mainAxisSize: MainAxisSize.max,
             children: [
               Expanded(
-                child: Builder(
-                  builder: (context) {
-                    final chatData = FFAppState()
-                        .chats
-                        .where((e) => e.chatId == widget.chatID)
-                        .toList();
-                    return SingleChildScrollView(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children:
-                            List.generate(chatData.length, (chatDataIndex) {
-                          final chatDataItem = chatData[chatDataIndex];
-                          return Builder(
-                            builder: (context) {
-                              final chatMessages =
-                                  chatDataItem.messages.toList();
-                              return ListView.builder(
-                                padding: EdgeInsets.zero,
-                                shrinkWrap: true,
-                                scrollDirection: Axis.vertical,
-                                itemCount: chatMessages.length,
-                                itemBuilder: (context, chatMessagesIndex) {
-                                  final chatMessagesItem =
-                                      chatMessages[chatMessagesIndex];
-                                  return Padding(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Builder(
+                        builder: (context) {
+                          final chatMessages = FFAppState()
+                              .messages
+                              .where((e) => e.messageChat == widget.chatID)
+                              .toList();
+                          return ListView.builder(
+                            padding: EdgeInsets.zero,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: chatMessages.length,
+                            itemBuilder: (context, chatMessagesIndex) {
+                              final chatMessagesItem =
+                                  chatMessages[chatMessagesIndex];
+                              return Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    15.0, 10.0, 15.0, 0.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: FlutterFlowTheme.of(context)
+                                        .secondaryBackground,
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  ),
+                                  child: Padding(
                                     padding: EdgeInsetsDirectional.fromSTEB(
-                                        15.0, 10.0, 15.0, 0.0),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: FlutterFlowTheme.of(context)
-                                            .secondaryBackground,
-                                        borderRadius:
-                                            BorderRadius.circular(5.0),
-                                      ),
-                                      child: Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            10.0, 10.0, 10.0, 10.0),
-                                        child: Row(
-                                          mainAxisSize: MainAxisSize.max,
-                                          children: [
-                                            Padding(
-                                              padding: EdgeInsetsDirectional
-                                                  .fromSTEB(
-                                                      0.0, 0.0, 15.0, 0.0),
-                                              child: Container(
-                                                width: 45.0,
-                                                height: 45.0,
-                                                decoration: BoxDecoration(
-                                                  color: FlutterFlowTheme.of(
-                                                          context)
+                                        10.0, 10.0, 10.0, 10.0),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  0.0, 0.0, 15.0, 0.0),
+                                          child: Container(
+                                            width: 45.0,
+                                            height: 45.0,
+                                            decoration: BoxDecoration(
+                                              color:
+                                                  FlutterFlowTheme.of(context)
                                                       .secondaryBackground,
-                                                ),
-                                                child: ClipRRect(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          5.0),
-                                                  child: Image.network(
-                                                    chatMessagesItem
-                                                        .messageSanderAvatar,
-                                                    width: 45.0,
-                                                    height: 5.0,
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                ),
+                                            ),
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(5.0),
+                                              child: Image.network(
+                                                chatMessagesItem
+                                                    .messageSanderAvatar,
+                                                width: 45.0,
+                                                height: 5.0,
+                                                fit: BoxFit.cover,
                                               ),
                                             ),
-                                            Column(
-                                              mainAxisSize: MainAxisSize.max,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  chatMessagesItem
-                                                      .messageSander,
-                                                  style: FlutterFlowTheme.of(
-                                                          context)
+                                          ),
+                                        ),
+                                        Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              chatMessagesItem.messageSander,
+                                              style:
+                                                  FlutterFlowTheme.of(context)
                                                       .titleSmall,
-                                                ),
-                                                Container(
-                                                  width:
-                                                      MediaQuery.sizeOf(context)
-                                                              .width *
-                                                          0.6,
-                                                  decoration: BoxDecoration(),
-                                                  child: Text(
-                                                    chatMessagesItem
-                                                        .messageBody,
-                                                    style: FlutterFlowTheme.of(
-                                                            context)
+                                            ),
+                                            Container(
+                                              width: MediaQuery.sizeOf(context)
+                                                      .width *
+                                                  0.6,
+                                              decoration: BoxDecoration(),
+                                              child: Text(
+                                                chatMessagesItem.messageBody,
+                                                style:
+                                                    FlutterFlowTheme.of(context)
                                                         .bodyMedium,
-                                                  ),
-                                                ),
-                                                Row(
-                                                  mainAxisSize:
-                                                      MainAxisSize.max,
-                                                  children: [
-                                                    Builder(
-                                                      builder: (context) {
-                                                        final asdasda =
-                                                            chatMessagesItem
-                                                                .messageReaded
-                                                                .toList();
-                                                        return Row(
-                                                          mainAxisSize:
-                                                              MainAxisSize.max,
-                                                          children: List.generate(
-                                                              asdasda.length,
-                                                              (asdasdaIndex) {
-                                                            final asdasdaItem =
-                                                                asdasda[
-                                                                    asdasdaIndex];
-                                                            return Text(
-                                                              asdasdaItem
-                                                                  .playerUid,
-                                                              style: FlutterFlowTheme
-                                                                      .of(context)
-                                                                  .bodyMedium,
-                                                            );
-                                                          }),
+                                              ),
+                                            ),
+                                            Row(
+                                              mainAxisSize: MainAxisSize.max,
+                                              children: [
+                                                Builder(
+                                                  builder: (context) {
+                                                    final asdasda =
+                                                        chatMessagesItem
+                                                            .messageReaded
+                                                            .toList();
+                                                    return Row(
+                                                      mainAxisSize:
+                                                          MainAxisSize.max,
+                                                      children: List.generate(
+                                                          asdasda.length,
+                                                          (asdasdaIndex) {
+                                                        final asdasdaItem =
+                                                            asdasda[
+                                                                asdasdaIndex];
+                                                        return Text(
+                                                          asdasdaItem.playerUid,
+                                                          style: FlutterFlowTheme
+                                                                  .of(context)
+                                                              .bodyMedium,
                                                         );
-                                                      },
-                                                    ),
-                                                  ],
+                                                      }),
+                                                    );
+                                                  },
                                                 ),
                                               ],
                                             ),
                                           ],
                                         ),
-                                      ),
+                                      ],
                                     ),
-                                  );
-                                },
+                                  ),
+                                ),
                               );
                             },
                           );
-                        }),
+                        },
                       ),
-                    );
-                  },
+                    ],
+                  ),
                 ),
               ),
               Padding(
