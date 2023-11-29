a3cebda09fb8b5b00afbd0c2659fbb42b67f0d53
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-29 12:07:23 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -1327,7 +1327,7 @@ class GetchatmessagesCall {
     return ApiManager.instance.makeApiCall(
       callName: 'GETCHATMESSAGES',
       apiUrl:
-          '${MessagingGroup.baseUrl}message?message_chat=eq.${chatId}&select=*,message_readed(*)',
+          '${MessagingGroup.baseUrl}message?message_chat=eq.${chatId}&select=*,message_readed(*,player_uid:players(*))',
       callType: ApiCallType.GET,
       headers: {
         'apikey':
