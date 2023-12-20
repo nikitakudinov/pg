910e367b5865f21da901034d538f43ece4dd49da
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-20 14:32:12 +0000

    Updating to latest FlutterFlow output.

--- lib/tournament/tournament_view/tournament_view_widget.dart
+++ lib/tournament/tournament_view/tournament_view_widget.dart
@@ -383,7 +383,7 @@ class _TournamentViewWidgetState extends State<TournamentViewWidget> {
                                 updateCallback: () => setState(() {}),
                                 child: TOURNAMENTgridROUNDWidget(
                                   round: 4,
-                                  iteamspacing: 500,
+                                  iteamspacing: 650,
                                   paddingtop: 0,
                                   itemheight: 100,
                                 ),
