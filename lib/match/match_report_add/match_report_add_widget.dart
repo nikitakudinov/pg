856bf051b7dd894747ca37e00493ad7e8a504d3b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-21 18:43:48 +0000

    Updating to latest FlutterFlow output.

--- lib/match/match_report_add/match_report_add_widget.dart
+++ lib/match/match_report_add/match_report_add_widget.dart
@@ -232,7 +232,7 @@ class _MatchReportAddWidgetState extends State<MatchReportAddWidget> {
                               itemBuilder: (context, matchIndex) {
                                 final matchItem = match[matchIndex];
                                 return Container(
-                                  height: 100.0,
+                                  height: 120.0,
                                   decoration: BoxDecoration(
                                     color: FlutterFlowTheme.of(context)
                                         .secondaryBackground,
