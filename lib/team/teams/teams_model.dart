784148654c68087d64974b87687a062f31000478
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-23 22:22:56 +0000

    Updating to latest FlutterFlow output.

--- lib/team/teams/teams_model.dart
+++ lib/team/teams/teams_model.dart
@@ -1,7 +1,9 @@
 import '/auth/supabase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
 import '/components/custom_alert_dialog/custom_alert_dialog_widget.dart';
+import '/components/screenshots_in_notification_widget.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -26,6 +28,8 @@ class TeamsModel extends FlutterFlowModel<TeamsWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // Stores action output result for [Backend Call - API (MATCHbyTORNandROUNDandPAIR)] action in Button widget.
+  ApiCallResponse? nextmatch;
 
   /// Initialization and disposal methods.
 
