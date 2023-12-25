0593b9f4763f4508b0b8c1dec28450bb1d8e4407
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-25 17:11:05 +0000

    Updating to latest FlutterFlow output.

--- lib/test2/test2_model.dart
+++ lib/test2/test2_model.dart
@@ -30,6 +30,10 @@ class Test2Model extends FlutterFlowModel<Test2Widget> {
   late GridRoundModel gridRoundModel8;
   // Model for GRID-ROUND component.
   late GridRoundModel gridRoundModel9;
+  // Model for GRID-ROUND component.
+  late GridRoundModel gridRoundModel10;
+  // Model for GRID-ROUND component.
+  late GridRoundModel gridRoundModel11;
 
   /// Initialization and disposal methods.
 
@@ -43,6 +47,8 @@ class Test2Model extends FlutterFlowModel<Test2Widget> {
     gridRoundModel7 = createModel(context, () => GridRoundModel());
     gridRoundModel8 = createModel(context, () => GridRoundModel());
     gridRoundModel9 = createModel(context, () => GridRoundModel());
+    gridRoundModel10 = createModel(context, () => GridRoundModel());
+    gridRoundModel11 = createModel(context, () => GridRoundModel());
   }
 
   void dispose() {
@@ -56,6 +62,8 @@ class Test2Model extends FlutterFlowModel<Test2Widget> {
     gridRoundModel7.dispose();
     gridRoundModel8.dispose();
     gridRoundModel9.dispose();
+    gridRoundModel10.dispose();
+    gridRoundModel11.dispose();
   }
 
   /// Action blocks are added here.
--- lib/test2/test2_widget.dart
+++ lib/test2/test2_widget.dart
@@ -106,7 +106,7 @@ class _Test2WidgetState extends State<Test2Widget> {
                           round: 2,
                           iteamspacing: 110,
                           paddingtop: 5,
-                          itemheight: 110,
+                          itemheight: 100,
                           pairFrom: 0,
                           pairTo: 4,
                           tournamentId: widget.tournamentId,
@@ -201,6 +201,43 @@ class _Test2WidgetState extends State<Test2Widget> {
                           pairFrom: 9,
                           pairTo: 16,
                           tournamentId: widget.tournamentId,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.sizeOf(context).width * 1.0,
+                  child: Stack(
+                    children: [
+                      wrapWithModel(
+                        model: _model.gridRoundModel10,
+                        updateCallback: () => setState(() {}),
+                        child: GridRoundWidget(
+                          round: 1,
+                          iteamspacing: 110,
+                          paddingtop: 5,
+                          itemheight: 110,
+                          pairFrom: 0,
+                          pairTo: 8,
+                          tournamentId: widget.tournamentId,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsetsDirectional.fromSTEB(80.0, 0.0, 0.0, 0.0),
+                        child: wrapWithModel(
+                          model: _model.gridRoundModel11,
+                          updateCallback: () => setState(() {}),
+                          child: GridRoundWidget(
+                            round: 2,
+                            iteamspacing: 110,
+                            paddingtop: 5,
+                            itemheight: 100,
+                            pairFrom: 0,
+                            pairTo: 4,
+                            tournamentId: widget.tournamentId,
+                          ),
                         ),
                       ),
                     ],
