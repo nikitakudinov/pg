f7c7511da746727c2de903ec7f5303ef56126571
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-25 17:59:46 +0000

    Updating to latest FlutterFlow output.

--- lib/test2/test2_widget.dart
+++ lib/test2/test2_widget.dart
@@ -189,7 +189,7 @@ class _Test2WidgetState extends State<Test2Widget> {
                                 updateCallback: () => setState(() {}),
                                 child: GridRoundWidget(
                                   round: 2,
-                                  iteamspacing: 120,
+                                  iteamspacing: 260,
                                   paddingtop: 5,
                                   itemheight: 110,
                                   pairFrom: 0,
