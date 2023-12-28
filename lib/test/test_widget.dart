ada8567da897294e90db401afaaa573445dcaf77
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-28 08:28:04 +0000

    Updating to latest FlutterFlow output.

--- lib/test/test_widget.dart
+++ lib/test/test_widget.dart
@@ -1,5 +1,3 @@
-import '/backend/schema/structs/index.dart';
-import '/components/vlist_i_t_e_m_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -87,99 +85,19 @@ class _TestWidgetState extends State<TestWidget> {
         ),
         body: SafeArea(
           top: true,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Builder(
-                  builder: (context) {
-                    final pLAYERSlist = FFAppState()
-                        .MAINDATA
-                        .players
-                        .toList()
-                        .take(10)
-                        .toList();
-                    return ListView.builder(
-                      padding: EdgeInsets.zero,
-                      primary: false,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      itemCount: pLAYERSlist.length,
-                      itemBuilder: (context, pLAYERSlistIndex) {
-                        final pLAYERSlistItem = pLAYERSlist[pLAYERSlistIndex];
-                        return Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Text(
-                              pLAYERSlistItem.playerNickname,
-                              style: FlutterFlowTheme.of(context).bodyMedium,
-                            ),
-                            Text(
-                              pLAYERSlistItem.playerTag,
-                              style: FlutterFlowTheme.of(context).bodyMedium,
-                            ),
-                          ].divide(SizedBox(width: 10.0)),
-                        );
-                      },
-                    );
-                  },
-                ),
-                Builder(
-                  builder: (context) {
-                    final tOURNAMENTSlist = FFAppState()
-                        .MAINDATA
-                        .tournaments
-                        .toList()
-                        .take(10)
-                        .toList();
-                    return ListView.builder(
-                      padding: EdgeInsets.zero,
-                      primary: false,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      itemCount: tOURNAMENTSlist.length,
-                      itemBuilder: (context, tOURNAMENTSlistIndex) {
-                        final tOURNAMENTSlistItem =
-                            tOURNAMENTSlist[tOURNAMENTSlistIndex];
-                        return InkWell(
-                          splashColor: Colors.transparent,
-                          focusColor: Colors.transparent,
-                          hoverColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          onTap: () async {
-                            context.pushNamed(
-                              'TEST2',
-                              queryParameters: {
-                                'tournamentId': serializeParam(
-                                  tOURNAMENTSlistItem.tournamentId,
-                                  ParamType.int,
-                                ),
-                              }.withoutNulls,
-                            );
-                          },
-                          child: VlistITEMWidget(
-                            key: Key(
-                                'Keyjqz_${tOURNAMENTSlistIndex}_of_${tOURNAMENTSlist.length}'),
-                            imageSize: 50,
-                            backgroundColor:
-                                FlutterFlowTheme.of(context).tertiary,
-                            title: tOURNAMENTSlistItem.tournamentTag,
-                            subtitle: tOURNAMENTSlistItem.tournamentName,
-                            country: tOURNAMENTSlistItem.tournamentCountry,
-                            flag: tOURNAMENTSlistItem.tournamentFlag,
-                            image: tOURNAMENTSlistItem.tournamentLogo,
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-                Text(
-                  FFAppState().MAINDATA.authplayer.playerNickname,
-                  style: FlutterFlowTheme.of(context).bodyMedium,
-                ),
-              ],
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                FFAppState().COUNTERS.teamslastupdatetime,
+                style: FlutterFlowTheme.of(context).bodyMedium,
+              ),
+              Text(
+                FFAppState().COUNTERS.updatedteams.toString(),
+                style: FlutterFlowTheme.of(context).bodyMedium,
+              ),
+            ],
           ),
         ),
       ),
