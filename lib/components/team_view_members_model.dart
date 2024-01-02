2d556a3bcd215275f29dd65743cec50b1dd7ad78
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-02 09:38:40 +0000

    Updating to latest FlutterFlow output.

--- lib/components/team_view_members_model.dart
+++ lib/components/team_view_members_model.dart
@@ -2,6 +2,7 @@ import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'team_view_members_widget.dart' show TeamViewMembersWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
--- lib/components/team_view_members_widget.dart
+++ lib/components/team_view_members_widget.dart
@@ -2,6 +2,7 @@ import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -171,7 +172,10 @@ class _TeamViewMembersWidgetState extends State<TeamViewMembersWidget> {
                         ),
                         Expanded(
                           child: Text(
-                            membersItem.playerTeamJoinedAt,
+                            dateTimeFormat(
+                                'relative',
+                                functions.stringDateToDateTime(
+                                    membersItem.playerTeamJoinedAt)),
                             style: FlutterFlowTheme.of(context).bodyMedium,
                           ),
                         ),
