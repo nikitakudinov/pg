c5fab03f65280afcc9f6537d4c65a44874a59501
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-09 11:36:34 +0000

    Updating to latest FlutterFlow output.

--- lib/chat/chats/chats_model.dart
+++ lib/chat/chats/chats_model.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/backend/schema/structs/index.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'chats_widget.dart' show ChatsWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
--- lib/chat/chats/chats_widget.dart
+++ lib/chat/chats/chats_widget.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/backend/schema/structs/index.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -245,7 +246,10 @@ class _ChatsWidgetState extends State<ChatsWidget> {
                                       mainAxisSize: MainAxisSize.max,
                                       children: [
                                         Text(
-                                          'Hello World',
+                                          dateTimeFormat(
+                                              'H m',
+                                              functions.stringDateToDateTime(
+                                                  chatsListItem.chatUpdatedAt)),
                                           style: FlutterFlowTheme.of(context)
                                               .bodyMedium,
                                         ),
