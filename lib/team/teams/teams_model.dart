0e5864dc63bcf36d351e8947033f8384007e2201
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-19 07:49:27 +0000

    Updating to latest FlutterFlow output.

--- lib/team/teams/teams_model.dart
+++ lib/team/teams/teams_model.dart
@@ -9,6 +9,7 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'teams_widget.dart' show TeamsWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
--- lib/team/teams/teams_widget.dart
+++ lib/team/teams/teams_widget.dart
@@ -9,6 +9,7 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -50,6 +51,12 @@ class _TeamsWidgetState extends State<TeamsWidget>
   void initState() {
     super.initState();
     _model = createModel(context, () => TeamsModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await action_blocks.teamsloader(context);
+      setState(() {});
+    });
   }
 
   @override
