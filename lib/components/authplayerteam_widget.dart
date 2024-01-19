a2253b9f7d904227857fc8e26a3e383dec8ae7f4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-19 15:24:47 +0000

    Updating to latest FlutterFlow output.

--- lib/components/authplayerteam_widget.dart
+++ lib/components/authplayerteam_widget.dart
@@ -8,7 +8,6 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/actions/actions.dart' as action_blocks;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -141,8 +140,6 @@ class _AuthplayerteamWidgetState extends State<AuthplayerteamWidget>
                                     currentUserUid,
                                   ),
                                 );
-                                await action_blocks.loadALLplayers(context);
-                                setState(() {});
                                 if (Navigator.of(context).canPop()) {
                                   context.pop();
                                 }
